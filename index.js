2dcec6c2f38d49054aa9381ca6cdca8ab33f293e
Jieun Lee <jieun@purdue.edu>
2018-06-04 21:14:59 -0400

    add two buttons and a heading

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('testing');
+const b1 = document.querySelector('.b1');
+const b2 = document.querySelector('.b2');
+const h1 = document.querySelector('h1');
+const h2 = document.querySelector('h2');
+
+const h1text = h1.textContent
+const h2text = h2.textContent
+
+b1.style.color = "blue";
+b2.style.color = "blue";
+
+b1.addEventListener('click', function(){
+    if(h1.textContent === h1text){
+        h1.textContent = "HEY!";
+        h1.style.color = "deeppink";
+        h1.style.backgroundColor = 'Gold';
+    }
+    else{
+        h1.textContent = h1text;
+        h1.style.color = "black";
+        h1.style.backgroundColor = 'transparent';
+    }
+    b1.style.color="purple";
+});
+
+b2.addEventListener('click', function(){
+    if(h2.textContent === h2text){
+        h2.textContent = "MAGIC";
+        h2.style.color = "Tomato";
+        h2.style.backgroundColor = "aqua";
+    }
+    else{
+        h2.textContent = "Good morning";
+        h2.style.color = "black";
+        h2.style.backgroundColor = "transparent";
+    }
+    b2.style.color = 'purple';
+});
+
+document.querySelector('fb').addEventListener('click', function(){
+    h1.textContent = document.querySelector('nh').textContent;
+})
